944d64fbe9645a54288f27831c453bf50088011d
Parthasarathy <mailtosarathy@gmail.com>
2025-10-30 09:40:54 +0530

    Added log message

--- pinmap.h
+++ pinmap.h
@@ -534,7 +534,11 @@ NexNumber  btnnumber2gp =NexNumber(1,19,"bt_1_bi2_uc_36");
 NexNumber  btnnumber3gp =NexNumber(1,22,"bt_1_emg_uc_32");
 
 void PinToDisplay(){
+    Serial.println("Reading Digital 1:");
     struct DigitalInput inputs=  digitalReadAll(MegaPinMap1);
+    
+    Serial.println("Completed Digital 1:");
+    Serial.println("Start Nextion Write 1:");
     btnnumber0p1.setValue(inputs.p0);
     btnnumber1p1.setValue(inputs.p1);
     btnnumber2p1.setValue(inputs.p2);
@@ -551,7 +555,9 @@ void PinToDisplay(){
     btnnumber13p1.setValue(inputs.p13);
     btnnumber14p1.setValue(inputs.p14);
     btnnumber15p1.setValue(inputs.p15);
+    Serial.println("Stop Nextion Write 1:");
 
+    Serial.println("Reading Digital 2:");
     inputs=  digitalReadAll(MegaPinMap2);
     btnnumber0p2.setValue(inputs.p0);
     btnnumber1p2.setValue(inputs.p1);
@@ -574,6 +580,7 @@ void PinToDisplay(){
     btnnumber1gp.setValue(digitalRead(GPMap[1].pin));
     btnnumber2gp.setValue(digitalRead(GPMap[2].pin));
     btnnumber3gp.setValue(digitalRead(GPMap[3].pin));
+    Serial.println("Completed Digital 2:");
 
     SetStatusText();
 }
